ddc2cbafa0353d0c6a268187403688fa4989a99a
gus17 <gustavopantoja.ap@gmail.com>
2023-11-24 17:21:19 -0300

    teste para envio

--- menu-legado/src/main.js
+++ menu-legado/src/main.js
@@ -1,8 +1,11 @@
-import Vue from 'vue'
+import './assets/main.css'
+
+import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
 
-Vue.config.productionTip = false
+const app = createApp(App)
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+app.use(router)
+
+app.mount('#app')
--- menu-legado/vue.config.js
+++ menu-legado/vue.config.js
@@ -1,4 +1,22 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+const path = require('path');
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+    transpileDependencies: true,
+    publicPath: './',
+
+    configureWebpack: {
+        module: {
+            rules: [
+                {
+                    test: /\.pdf$/,
+                    loader: 'file-loader',
+                    options: {
+                        name: 'pdfs/[name].[hash:8].[ext]',
+                    },
+                },
+            ],
+        },
+    },
+});
+
