1c92dc38157378709fd253f6b9600a8b241944a9
user_cicd <frank@infoconsig.com.br>
2024-01-31 04:10:17 +0000

    feat/review: add componente de data

--- menu-legado/src/views/components/consultas/consultas-datatable/consultas-datatable.vue
+++ menu-legado/src/views/components/consultas/consultas-datatable/consultas-datatable.vue
@@ -1,9 +1,19 @@
 <script lang="ts" setup>
+	import flatPickr from 'vue-flatpickr-component'
+	import monthSelectPlugin from 'flatpickr/dist/plugins/monthSelect/index.js'
 	import Vue3Datatable from '@bhplugin/vue3-datatable'
 	import { reactive, ref } from 'vue'
 
+	import { Portuguese } from 'flatpickr/dist/l10n/pt.js'
+
 	// Componentes
+	import ConsultasExport from '../consultas-export/consultas-export.vue'
 	import modalLayout from '@components/layout/modalLayout.vue'
+
+	// Icons
+	import IconClear from '@icons/iconClear.vue'
+	import IconFile from '@icons/iconFile.vue'
+	import IconPrinter from '@icons/iconPrinter.vue'
 
 	// Props
 	const props = defineProps({
@@ -23,6 +33,7 @@
 
 	// Declaração
 	const isOpenDialog = ref<boolean>(false)
+	const dateSelected = ref<string>('')
 	const columnsModal = reactive([
 		{ field: 'rmc', title: 'RMC', hide: false },
 		{ field: 'data', title: 'Data', hide: false },
@@ -86,6 +97,15 @@
 			},
 		},
 	])
+	const flatPickrConfig = reactive({
+		locale: Portuguese,
+		plugins: [
+			monthSelectPlugin({
+				shorthand: true,
+				dateFormat: 'M/Y',
+			}),
+		],
+	})
 
 	// Scripts
 	const color = (id: number | string): string => {
@@ -103,6 +123,16 @@
 		}
 	}
 
+	const clearFilter = () => {
+		servico.value = ''
+		consignataria.value = ''
+		status.value = ''
+		dateSelected.value = ''
+
+		selected.label = ''
+		selected.type = ''
+	}
+
 	const filtered = (value: string = '') => {
 		if (value === '') return props.rows
 
@@ -114,6 +144,9 @@
 
 		if (props.selected.type === 'situacao')
 			return props.rows.filter((item: any) => item.status.label === value)
+
+		if (props.selected.type === 'date')
+			return props.rows.filter((item: any) => item.data === value)
 	}
 
 	// fonte: https://vue3-datatable-document.vercel.app/skeleton-loader
@@ -153,37 +186,122 @@
 		btn-close
 		@btn-close="isOpenDialog = false"
 	>
-		<!-- Datatable-->
-		<div class="datatable">
-			<vue3-datatable
-				:rows="rowsModal"
-				:columns="columnsModal"
-				:total-rows="rowsModal.length"
-				:sortable="true"
-				skin="whitespace-nowrap bh-table-striped"
-				no-data-content="Nenhum dado foi encontrado"
-				pagination-info="Mostrando {0} a {1} de {2} entradas"
+		<div
+			class="flex flex-wrap justify-between md:items-center md:flex-row flex-col mb-5 gap-5"
+		>
+			<div
+				class="header_actions w-full flex justify-end items-center gap-5 ltr:ml-auto rtl:mr-auto"
 			>
-				<template #rmc="data">
-					<button>
-						<strong class="text-primary_3-table">{{ data.value.rmc }}</strong>
-					</button>
-				</template>
-				<template #n_contrato="data">
-					<button>
-						<strong class="text-primary_3-table">{{
-							data.value.n_contrato
-						}}</strong>
-					</button>
-				</template>
-				<template #status="data">
-					<span
-						class="flex justify-center badge !w-[80px] h-[22px]"
-						:class="color(data.value.status.id)"
-						>{{ data.value.status.label }}</span
+				<flat-pickr
+					v-model="dateSelected"
+					class="form-input form-select max-w-[120px]"
+					placeholder="Selecione um data"
+					:config="flatPickrConfig"
+					@change="
+						(selected.label = $event.target.value), (selected.type = 'date')
+					"
+				/>
+				<multiselect
+					v-model="servico"
+					:options="[
+						'Plano de Saúde',
+						'Mensalidade',
+						'Empréstimos',
+						'Compras',
+						'Compras/Saque',
+					]"
+					class="custom-multiselect max-w-[230px]"
+					placeholder="Selecione o tipo de serviço"
+					:searchable="false"
+					:preselect-first="false"
+					:allow-empty="false"
+					selected-label=""
+					select-label=""
+					deselect-label=""
+					@select="(selected.label = $event), (selected.type = 'servico')"
+				/>
+				<multiselect
+					v-model="status"
+					:options="['Encerrado', 'Suspenso', 'Ativo']"
+					class="custom-multiselect max-w-[200px]"
+					placeholder="Selecione a Situação"
+					:searchable="false"
+					:preselect-first="false"
+					:allow-empty="false"
+					selected-label=""
+					select-label=""
+					deselect-label=""
+					@select="(selected.label = $event), (selected.type = 'situacao')"
+				/>
+				<div>
+					<button
+						v-tippy:top
+						type="button"
+						class="text-xs m-1"
+						@click="clearFilter()"
 					>
-				</template>
-			</vue3-datatable>
+						<icon-clear class="w-5 h-5 text-primary_3-table" />
+					</button>
+					<tippy target="top" placement="top">Limpar pesquisa</tippy>
+				</div>
+				<div>
+					<download-excel
+						v-tippy:top
+						:fields="columns"
+						:data="rows"
+						name="resumo-dos-contratos.xls"
+						class="text-xs m-1 cursor-pointer"
+					>
+						<icon-file class="w-5 h-5" />
+					</download-excel>
+					<tippy target="top" placement="top">Exportar XLS</tippy>
+				</div>
+				<div>
+					<consultas-export
+						v-tippy:top
+						:cols="cols"
+						:rows="rows"
+						export-type="print"
+					>
+						<template #icon>
+							<icon-printer class="w-5 h-5" />
+						</template>
+					</consultas-export>
+					<tippy target="top" placement="top">Imprimir</tippy>
+				</div>
+			</div>
+			<!-- Datatable-->
+			<div class="datatable">
+				<vue3-datatable
+					:rows="rowsModal"
+					:columns="columnsModal"
+					:total-rows="rowsModal.length"
+					:sortable="true"
+					skin="whitespace-nowrap bh-table-striped"
+					no-data-content="Nenhum dado foi encontrado"
+					pagination-info="Mostrando {0} a {1} de {2} entradas"
+				>
+					<template #rmc="data">
+						<button>
+							<strong class="text-primary_3-table">{{ data.value.rmc }}</strong>
+						</button>
+					</template>
+					<template #n_contrato="data">
+						<button>
+							<strong class="text-primary_3-table">{{
+								data.value.n_contrato
+							}}</strong>
+						</button>
+					</template>
+					<template #status="data">
+						<span
+							class="flex justify-center badge !w-[80px] h-[22px]"
+							:class="color(data.value.status.id)"
+							>{{ data.value.status.label }}</span
+						>
+					</template>
+				</vue3-datatable>
+			</div>
 		</div>
 		<!-- Datatable-->
 	</modal-layout>
@@ -197,5 +315,22 @@
 		font-style: normal;
 		font-weight: 600;
 		line-height: normal;
+	}
+
+	.header_actions:deep(.custom-multiselect) {
+		.multiselect__placeholder {
+			font-size: 0.75rem;
+			line-height: 1rem;
+			font-weight: 600;
+			white-space: nowrap;
+			color: rgb(14 23 38);
+		}
+	}
+
+	.flatpickr-input::placeholder {
+		font-size: 0.75rem;
+		font-weight: 600;
+		white-space: nowrap;
+		color: rgb(14 23 38);
 	}
 </style>
