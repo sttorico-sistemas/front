c65b894b5a84a7a7ccb90ed061cc1c0a0d5d1d73
Moises <moises_system@yahoo.com.br>
2024-02-29 12:21:14 +0000

    criação de componentes

---
+++ src/components/layout/inputValidation.vue
@@ -0,0 +1,55 @@
+<script setup>
+	defineProps({
+		id: {
+			type: String,
+			default: '',
+		},
+		label: {
+			type: String,
+			default: '',
+		},
+		type: {
+			type: String,
+			default: 'text',
+		},
+		placeholder: {
+			type: String,
+			default: '',
+		},
+		styleClass: {
+			type: String,
+			default: '',
+		},
+		messageError: {
+			type: String,
+			default: '',
+		},
+		isSubmitForm: {
+			type: Boolean,
+			default: false,
+		},
+		error: {
+			type: Boolean,
+			default: false,
+		},
+	})
+</script>
+<template>
+	<div
+		:class="{
+			'has-error': error,
+		}"
+	>
+		<label for="{{ id }}" class="text-xs">{{ label }}</label>
+		<input
+			:id="id"
+			:type="type"
+			:placeholder="placeholder"
+			class="form-input"
+			:class="styleClass"
+		/>
+		<div v-if="isSubmitForm && error">
+			<p class="text-danger mt-1">{{ messageError }}</p>
+		</div>
+	</div>
+</template>
---
+++ src/components/layout/stepByNumber.vue
@@ -0,0 +1,33 @@
+<script setup>
+	import { computed } from 'vue'
+
+	const props = defineProps({
+		value: {
+			type: String,
+			required: true,
+		},
+		size: {
+			type: Number,
+			default: 5,
+		},
+		bgColor: {
+			type: String,
+			default: 'bg-info',
+		},
+		textColor: {
+			type: String,
+			default: 'text-white',
+		},
+	})
+
+	const size = computed(() => {
+		return `size-${props.size}`
+	})
+</script>
+<template>
+	<span
+		class="rounded-full flex justify-center items-center font-medium py-[1px] px-[7px]"
+		:class="[size, props.bgColor, props.textColor]"
+		>{{ value }}</span
+	>
+</template>
