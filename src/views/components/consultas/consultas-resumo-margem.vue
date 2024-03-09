726990d538163e94e239f9ffbb78c8076623dc4d
Moises <moises_system@yahoo.com.br>
2024-03-09 23:14:22 +0000

    feat: cria compoente para consultas resumo margem

--- src/views/components/consultas/consultas-resumo-margem.vue
+++ src/views/components/consultas/consultas-resumo-margem.vue
@@ -5,21 +5,21 @@
 
 	// Componentes
 	import breadcrumbs from '@components/layout/breadcrumbsLayout.vue'
+	import loadingFixed from '@components/layout/loadingFixed.vue'
 	import modalLayout from '@components/layout/modalLayout.vue'
 
 	import ConsultasExport from './consultas-export/consultas-export.vue'
 	import ConsultasTitulo from './consultas-titulo/consultas-titulo.vue'
+	import ConsultaResumoMargemPrincipal from './consultas-resumo-margem/consultas-resumo-margem-principal/consultas-resumo-margem-principal.vue'
+	import ConsultaSaldoDisponivelMargem from './consultas-resumo-margem/consultas-saldo-disponivel-margem.vue'
 
 	// Icons
-	import calculatorIcon from '@icons/iconCalculator2.vue'
 	import IconEye from '@icons/iconEye.vue'
 	import IconPrinter from '@icons/iconPrinter.vue'
 
 	// Declarações
 	const isOpenDialog = ref<boolean>(false)
 	const dataMargemAtualizada = ref<string>('23/10/2023')
-	const optionsServicos = ref(['Servico 1', 'Servico 2', 'Servico 3'])
-	const servico = ref()
 	const cols = reactive([
 		{ field: 'tipo_margem', title: 'Tipo da Margem', hide: false, sort: false, },
 		{ field: 'limite', title: 'Limite %', hide: false, sort: false, },
@@ -64,22 +64,7 @@
 	])
 
 	// Scripts
-	const color = (value: number) => {
-		switch (value) {
-			case 1:
-				return 'bg-dark-primary'
-			case 2:
-				return 'bg-info'
-			case 3:
-				return 'bg-success'
-			case 4:
-				return 'bg-warning'
-		}
-	}
-
 	const openModal = (id: number) => isOpenDialog.value = true
-
-	const clear = () => true
 
 	const formatedCurrency = (value: number) => {
 		return value.toLocaleString('pt-br', {
@@ -95,13 +80,20 @@
 
 		<div class="panel mt-6">
 			<div class="flex flex-wrap justify-between md:items-center md:flex-row flex-col mb-5 gap-5">
-				<consultas-titulo title="Resumo da Margem Consignável"
-					info-message="Alguma mensagem aqui para tirar dúvida sobre a página" />
+				<consultas-titulo 
+					title="Resumo da Margem Consignável"
+					info-message="Alguma mensagem aqui para tirar dúvida sobre a página" 
+				/>
 
 				<div class="header_actions flex items-center gap-5 ltr:ml-auto rtl:mr-auto">
 					<div class="w-5 h-5">
-						<consultas-export v-tippy:top :cols="cols" :rows="rows" filename="Extrato Anual dos Descontos"
-							export-type="print">
+						<consultas-export 
+							v-tippy:top 
+							:cols="cols" 
+							:rows="rows" 
+							filename="Extrato Anual dos Descontos"
+							export-type="print"
+						>
 							<template #icon>
 								<icon-printer class="w-5 h-5" />
 							</template>
@@ -120,20 +112,33 @@
 							class="ml-3 font-xs font-semibold text-[#2196F3]">{{ dataMargemAtualizada }}</strong></span>
 				</div>
 
-				<vue3-datatable :rows="rows" :columns="cols" :total-rows="rows.length" :sortable="true"
-					skin="whitespace-nowrap bh-table-striped mb-5" no-data-content="Nenhum dado foi encontrado"
-					pagination-info="Mostrando {0} a {1} de {2} entradas" :pagination="false">
+				<vue3-datatable 
+					:rows="rows" 
+					:columns="cols" 
+					:total-rows="rows.length" 
+					:sortable="true"
+					skin="whitespace-nowrap bh-table-striped mb-5" 
+					no-data-content="Nenhum dado foi encontrado"
+					pagination-info="Mostrando {0} a {1} de {2} entradas" 
+					:pagination="false"
+				>
 					<template #uso_margem="data">
 						<div class="flex justify-between items-center gap-3">
 							<div class="w-10/12 rounded-full h-2 bg-dark-light shadow">
-								<div class="h-full rounded-full" :class="color(data.value.id)"
-									:style="`width: ${data.value.uso_margem}%`" />
+								<loading-fixed
+									:color="data.value.id"
+									:percent="data.value.uso_margem" 
+								/>
 							</div>
 							<span class="text-xs">{{ formatedCurrency(data.value.uso_margem) }}%</span>
 						</div>
 					</template>
 					<template #action="data">
-						<button v-tippy:right type="button" class="text-xs m-1" @click="openModal(data.value.id)">
+						<button 
+							v-tippy:right 
+							type="button" 
+							class="text-xs m-1" 
+							@click="openModal(data.value.id)">
 							<icon-eye class="w-5 h-5 text-primary_3-table" />
 						</button>
 						<tippy target="right" placement="right">Ver resumo da margem principal</tippy>
@@ -142,53 +147,17 @@
 			</div>
 		</div>
 
-		<div class="panel pb-0 mt-6">
-			<div class="flex flex-wrap justify-between md:items-center md:flex-row flex-col mb-3 gap-5">
-				<consultas-titulo title="Saldo Disponível da Margem"
-					info-message="Alguma mensagem aqui para tirar dúvida sobre a página" />
-			</div>
+		<consulta-saldo-disponivel-margem />
 
-			<div id="saldo_disponivel" class="flex justify-between items-center py-8">
-				<multiselect 
-					v-model="servico" 
-					:options="optionsServicos" 
-					class="custom-multiselect w-[286px] h-[48px]" 
-					:searchable="false"
-					placeholder="Selecione Tipo de Serviço" 
-					:allow-empty="false" 
-					selected-label="" 
-					select-label=""
-					deselect-label="" 
-					optionHeight="48"
-				/>
-				
-				<div class="saldo_disponivel__valor self-end border rounded px-2 py-2">R$ 0,00</div>		
-				
-				<button
-					type="button"
-					class="w-[77px] h-[48px] btn border-primary_3-table text-primary_3-table shadow-none"
-					@click="clear"
-				>
-					Limpar
-				</button>
-
-				<div class="w-[575px] h-[48px] flex relative items-stretch flex-wrap">
-					<div class="ltr:-mr-px rtl:-ml-px flex">
-						<span
-							class="border border-[#e0e6ed] rounded-l bg-[#EAEAEC] flex items-center justify-center text-black px-4 py-1.5 ">
-							<calculatorIcon />
-						</span>
-					</div>
-					<span class="flex-1 flex items-center border rounded-r px-2 calcula_saldo">Calcule o saldo disponível da sua
-						Margem</span>
-				</div>
-			</div>
-		</div>
-
-		<modal-layout :is-open="isOpenDialog" title="Extrato Mensal dos Descontos" size="max-w-full" btn-close
-			@btn-close="isOpenDialog = false">
+		<modal-layout 
+			:is-open="isOpenDialog" 
+			title="Resumo da Margem Principal" 
+			size="w-[415px]" 
+			btn-close
+			@btn-close="isOpenDialog = false"
+		>
 			<!-- Datatable-->
-
+			<consulta-resumo-margem-principal />
 			<!-- Datatable-->
 		</modal-layout>
 	</main>
