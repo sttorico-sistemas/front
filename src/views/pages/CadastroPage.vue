5c694a7beb4c379ab53552b4078aa76e2b29c458
Moises <moises_system@yahoo.com.br>
2024-04-04 03:37:45 +0000

    feat: criação da Page para Cadastro

---
+++ src/views/pages/CadastroPage.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup>
+	import { useRoute } from 'vue-router'
+	import { onMounted, shallowRef, watch } from 'vue'
+
+	import cadastroConsignante from '../components/cadastro/cadastro-consignante.vue'
+
+	const route = useRoute()
+
+	const currentComponent = shallowRef<any>(null)
+
+	watch([route, currentComponent], () => {
+		switch (route.name) {
+			case 'cadastro-consignante':
+				currentComponent.value = cadastroConsignante
+				break
+		}
+	})
+
+	onMounted(() => {
+		currentComponent.value = route.name
+	})
+</script>
+
+<template>
+	<component :is="currentComponent" />
+</template>
