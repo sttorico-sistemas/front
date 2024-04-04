9ba6913532680ab1a94cd00e5d4e1d149696334d
Moises <moises_system@yahoo.com.br>
2024-04-04 03:38:21 +0000

    feat/WIP: criação do componente para cadastro consignante

---
+++ src/views/components/cadastro/cadastro-consignante.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+	
+	// Componentes
+	import breadcrumbs from '@components/layout/breadcrumbsLayout.vue'
+	
+	// Icons
+
+	// Declarações
+
+	// Script
+
+</script>
+<template>
+	<main>
+		<breadcrumbs :paginas="['Cadastro', 'Consignante']" />
+
+    <div class="panel mt-6">Dados do Consignante</div>
+    <div class="panel mt-6">Contratos Sistema</div>
+    <div class="panel mt-6">Lista de Gestores</div>
+    <div class="panel mt-6">Lista de Averbadores</div>
+    <div class="panel mt-6">Consignatárias Habilitadas</div>
+    <div class="panel mt-6">Logs de Alterações</div>
+	</main>
+</template>
+<style lang="scss" scoped></style>
