982e3fdfa32a799e84da729d2459bd444cf9c861
Moises <moises_system@yahoo.com.br>
2024-04-06 05:00:59 +0000

    feat: finalização do componente de modal para cadastro

--- src/views/components/cadastro/cadastro-modal-consignante/cadastro-modal-consignante.vue
+++ src/views/components/cadastro/cadastro-modal-consignante/cadastro-modal-consignante.vue
@@ -4,7 +4,7 @@
 	
 	// Componentes
   import LabelInput from '@components/layout/forms/inputs/inputLabel.vue'
-	import titulo from '@components/layout/tituloLayout.vue'
+  import LabelSelect from '@components/layout/forms/inputs/selectLabel.vue'
 	
 	// Icons
   import IconClear from '@icons/iconClear.vue'
@@ -20,13 +20,13 @@
 </script>
 <template>
 	<main>
-    <div class="panel mt-5 border border-primary_3-table flex items-center gap-12">
-      <label for="consignante_master" class="text-base text-primary_3-table m-0">Consignante Master</label>
+    <div class="panel mt-5 border border-primary_3-table flex items-center md:gap-12">
+      <label for="consignante_master" class="text-sm md:text-base text-primary_3-table m-0">Consignante Master</label>
       <div class="flex items-center gap-6">
         <select 
           v-model="consignanteMaster" 
           id="consignante_master" 
-          class="form-select w-[500px] h-[38px] font-normal" 
+          class="form-select w-auto md:w-[500px] h-[38px] font-normal" 
           required
         >
           <option value="">Selecione uma opção</option>
@@ -41,7 +41,70 @@
     </div>
 
     <div class="panel border mt-5 border-primary_3-table">
-      <h3 class="text-base font-semibold text-primary_3-table m-0">Consignante Master</h3>
+      <h3 class="text-base font-semibold text-primary_3-table mb-7">Consignante Master</h3>
+
+      <div class="flex flex-col md:flex-row items-center mb-3">
+        <label for="cnpj" class="mb-0 md:mr-2 w-full md:w-1/5 text-sm text-left">CNPJ</label>
+        <input id="cnpj" type="text" class="form-input mb-3 md:mb-0 md:max-w-[200px] h-[38px]" />        
+
+        <label for="tipo_entidade" class="mb-0 md:ml-3 md:mr-2 w-full md:w-auto text-sm text-left">Tipo de Entidade</label>
+        <select id="tipo_entidade" class="form-select md:max-w-[250px] h-[38px]">
+          <option value=""></option>
+          <option value="Entidade 1">Entidade 1</option>
+          <option value="Entidade 2">Entidade 2</option>
+          <option value="Entidade 3">Entidade 3</option>
+          <option value="Entidade 4">Entidade 4</option>
+        </select>
+      </div>
+
+      <label-input 
+        id="nome"
+        label="Nome"
+        class-label="text-sm"
+        class-input="md:max-w-[420px]"
+        layout="col"
+      />
+      <label-input 
+        id="nome_curto"
+        label="Nome curto"
+        class-label="text-sm"
+        class-input="md:max-w-[250px]"
+        layout="col"
+      />
+      <label-select
+        id="tipo_endereco"
+        label="Tipo de Endereço"
+        class-label="text-sm"
+        class-select="md:max-w-[250px]"
+        layout="col"
+        :options="['Av', 'Alameda', 'Beco', 'Comunidade', 'Estrada', 'Rua']"
+      />
+      <label-input 
+        id="endereco"
+        label="Endereço Completo"
+        class-label="text-sm"
+        class-input="md:max-w-[620px]"
+        layout="col"
+      />
+
+      <div class="flex flex-col md:flex-row items-center mb-3">
+        <label for="cidade" class="mb-0 md:mr-2 w-full md:w-1/5 text-sm text-left">Cidade</label>
+        <input id="cidade" type="text" class="form-input mb-3 md:mb-0 md:max-w-[250px] h-[38px]" />   
+
+        <label for="uf" class="mb-0 md:mr-4 md:ml-8 w-full md:w-auto text-sm text-left">UF</label>
+        <input id="uf" type="text" class="form-input mb-3 md:mb-0 md:max-w-[50px] h-[38px]" />  
+
+        <label for="cep" class="mb-0 md:mr-4 md:ml-8 w-full md:w-auto text-sm text-left">CEP</label>
+        <input id="cep" type="text" class="form-input mb-3 md:mb-0 md:max-w-[150px] h-[38px]" />   
+      </div>
+
+      <div class="flex flex-col md:flex-row items-center mb-3">
+        <label for="expediente" class="mb-0 md:mr-2 w-full md:w-1/5 text-sm text-left">Expediente</label>
+        <input id="expediente" type="text" class="form-input mb-3 md:mb-0 md:max-w-[80px] h-[38px]" />  
+
+        <label for="ate" class="mb-0 md:mx-2 w-full md:w-auto text-sm text-left">Até</label>
+        <input id="ate" type="text" class="form-input md:max-w-[80px] h-[38px]" />   
+      </div>
 
       <div class="flex justify-center items-center gap-12 mt-8">
         <button 
