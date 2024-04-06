13a0e88183f2a7bb93e35114ae119c257b2e64e7
Moises <moises_system@yahoo.com.br>
2024-04-06 05:00:15 +0000

    feat: melhoria em componente de input

--- src/components/layout/forms/inputs/inputLabel.vue
+++ src/components/layout/forms/inputs/inputLabel.vue
@@ -36,10 +36,18 @@
       type: Boolean,
       default: false,
     },
+    required: {
+      type: Boolean,
+      default: false,
+    },
+    layout: {
+      type: String,
+      default: 'row',
+    },
   })
 </script>
 <template>
-  <div :class="props.classWrapper">
+  <div v-if="props.layout === 'row'" :class="props.classWrapper">
     <label for="props.id" :class="props.classLabel">{{ props.label }}</label>
     <input 
       :id="props.id" 
@@ -48,6 +56,19 @@
       :class="props.classInput" 
       :value="props.value"
       :disabled="props.disabled" 
+      :required="props.required"
+    />
+  </div>
+  <div v-if="props.layout === 'col'" class="flex sm:flex-row flex-col items-center mb-3">
+    <label :for="props.id" class="mb-0 sm:w-1/5 sm:mr-2 w-full text-left" :class="props.classLabel">{{ props.label }}</label>
+    <input 
+      :id="props.id" 
+      :type="props.type" 
+      :placeholder="props.placeholder" 
+      class="form-input flex-1" 
+      :class="props.classInput" 
+      :disabled="props.disabled" 
+      :required="props.required"
     />
   </div>
 </template>
