06cc1f7f19e7efdaac8fdc7b99ff53e4f21f1775
Moises <moises_system@yahoo.com.br>
2024-04-17 04:28:26 +0000

    feat/wip: criar componente main para consignataria

---
+++ src/views/components/cadastro/cadastro-consignataria.vue
@@ -0,0 +1,32 @@
+<script lang="ts" setup>
+
+	// Componentes
+	import breadcrumbs from '@components/layout/breadcrumbsLayout.vue'
+
+	import ConsignatariaHabilitadas from './cadastro-consignataria-habilitadas/cadastro-consignataria-habilitadas.vue'
+	import ContratoSistema from './cadastro-contrato-sistema/cadastro-contrato-sistema.vue'
+	import DadosConsignataria from './cadastro-dados-consignataria/cadastro-dados-consignataria.vue'
+	import ListaAverbadores from './cadastro-lista-averbadores/cadastro-lista-averbadores.vue'
+	import ListaGestores from './cadastro-lista-gestores/cadastro-lista-gestores.vue'
+	import LogsAlteracao from './logs-alteracao/logs-alteracao.vue'
+
+	// Icons
+
+	// Declarações
+
+	// Script
+
+</script>
+<template>
+	<main>
+		<breadcrumbs :paginas="['Cadastro', 'Consignatária']" />
+
+    <dados-consignataria />
+    <contrato-sistema />
+    <lista-gestores />
+    <lista-averbadores />
+    <consignataria-habilitadas />
+    <logs-alteracao />
+	</main>
+</template>
+<style lang="scss" scoped></style>
