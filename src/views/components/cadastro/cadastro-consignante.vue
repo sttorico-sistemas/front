eff2ee658d7edfe11cb55049d6daa9adf2782df3
Moises <moises_system@yahoo.com.br>
2024-04-18 04:21:30 +0000

    ref: melhoria de componente para adptação generica

--- src/views/components/cadastro/cadastro-consignante.vue
+++ src/views/components/cadastro/cadastro-consignante.vue
@@ -1,5 +1,6 @@
 <script lang="ts" setup>
-	
+	import { ref, reactive } from 'vue'
+
 	// Componentes
 	import breadcrumbs from '@components/layout/breadcrumbsLayout.vue'
 
@@ -9,10 +10,37 @@
 	import ListaAverbadores from './cadastro-lista-averbadores/cadastro-lista-averbadores.vue'
 	import ListaGestores from './cadastro-lista-gestores/cadastro-lista-gestores.vue'
 	import LogsAlteracao from './logs-alteracao/logs-alteracao.vue'
-	
+
 	// Icons
 
 	// Declarações
+	const colsContratoSistema = reactive([
+		{ field: 'cod', title: 'Cód. Contrato', hide: false, sort: false, },
+		{ field: 'tipo_contrato', title: 'Tipo Contrato', hide: false, sort: false, },
+		{ field: 'vigencia', title: 'Vigência', hide: false, sort: false, },
+		{ field: 'data_inicial', title: 'Data Inicial', hide: false, sort: false, },
+		{ field: 'data_final', title: 'Data Final', hide: false, sort: false, },
+		{ field: 'status', title: 'Status', hide: false, sort: false, },
+		{ field: 'actions', title: 'Ações', hide: false, sort: false, },
+	])
+	const rowsContratoSistema = reactive([
+		{
+      cod: '55622',
+      tipo_contrato: 'Contrato Comodato',
+      vigencia: '48',
+      data_inicial: '23/03/2018',
+      data_final: '23/03/2023',
+      status: 'Ativo',
+    },
+    {
+      cod: '55632',
+      tipo_contrato: 'Acordo de Cooperação Técnica',
+      vigencia: '60',
+      data_inicial: '23/03/2018',
+      data_final: '23/03/2023',
+      status: 'Inativo',
+    },
+	])
 
 	// Script
 
@@ -22,7 +50,10 @@
 		<breadcrumbs :paginas="['Cadastro', 'Consignante']" />
 
     <dados-consignante />
-    <contrato-sistema />
+    <contrato-sistema
+			:cols="colsContratoSistema"
+			:rows="rowsContratoSistema"
+		/>
     <lista-gestores />
     <lista-averbadores />
     <consignataria-habilitadas />
