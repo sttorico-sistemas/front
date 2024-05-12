b08c784a36495349ff5c9771946d8e470506a7e1
Moises <moises_system@yahoo.com.br>
2024-05-12 02:47:13 +0000

    feat: add novo componente de icone

---
+++ src/components/icons/iconClose.vue
@@ -0,0 +1,7 @@
+<template>
+<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+  <path d="M12 21C16.9706 21 21 16.9706 21 12C21 7.02944 16.9706 3 12 3C7.02944 3 3 7.02944 3 12C3 16.9706 7.02944 21 12 21Z" stroke="#E7515A" stroke-width="2" stroke-miterlimit="10"/>
+  <path d="M8.76953 8.96002L15.2295 15.04" stroke="#E7515A" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round"/>
+  <path d="M15.2295 8.96002L8.76953 15.04" stroke="#E7515A" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round"/>
+</svg>
+</template>
