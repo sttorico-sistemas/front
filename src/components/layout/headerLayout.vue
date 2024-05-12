d7294f8d158b1a3c8809ec53f7c00eb11341d870
Moises <moises_system@yahoo.com.br>
2024-05-12 02:47:31 +0000

    feat: adicionar ao menu item de pessoas cadastradas

--- src/components/layout/headerLayout.vue
+++ src/components/layout/headerLayout.vue
@@ -201,6 +201,14 @@
 										Consignantes
 									</router-link>
 								</li>
+								<li>
+									<router-link
+										:to="{ name: 'consultas-pessoas' }"
+										class="block px-4 py-2 hover:bg-gray-100"
+									>
+										Pessoas Cadastradas
+									</router-link>
+								</li>
 								<hr class="mx-2">
 								<li>
 									<router-link
@@ -210,7 +218,6 @@
 										Consignatária Admin
 									</router-link>
 								</li>
-
 							</ul>
 						</div>
 					</button>
