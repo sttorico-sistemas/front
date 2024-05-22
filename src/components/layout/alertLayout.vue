c66a4c4df6dd83d65646a60a93c25ba543f91c38
Moises <moises_system@yahoo.com.br>
2024-05-22 04:19:20 +0000

    feat/wip: criação de eventBus para alert

---
+++ src/components/layout/alertLayout.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+  import { reactive, ref } from 'vue'
+
+  // Icons
+  import IconClose from '@icons/iconClose.vue'
+</script>
+<template>
+  <div class="flex items-center p-3.5 rounded text-danger bg-danger-light absolute right-5 bottom-5 z-[9999]">
+    <span class="pr-2 pl-2">
+      <strong class="mr-1 ml-1">Danger!</strong>Lorem Ipsum is simply dummy text of the printing.
+    </span>
+    <button type="button" class="ml-auto mr-auto hover:opacity-80">
+        <icon-close class="w-5 h-5" />
+    </button>
+  </div>
+</template>
---
+++ src/eventBus.ts
@@ -0,0 +1,6 @@
+import App from './App.vue'
+import { createApp } from 'vue'
+
+const eventBus = createApp(App)
+
+export default eventBus
