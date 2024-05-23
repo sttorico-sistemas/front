9660981f849cbdf8e054ed346e11f15a45e9a86e
Moises <moises_system@yahoo.com.br>
2024-05-23 05:37:16 +0000

    feat: adicionar eventBus para recebimento de mensagem

--- src/components/layout/alertLayout.vue
+++ src/components/layout/alertLayout.vue
@@ -1,13 +1,30 @@
 <script lang="ts" setup>
-  import { reactive, ref } from 'vue'
+  import { reactive, ref, onMounted, inject } from 'vue'
 
   // Icons
   import IconClose from '@icons/iconClose.vue'
+
+  // Declarações
+  const eventBus = inject('eventBus')
+  const message = ref<string>('')
+
+  // Scripts
+  // remover apos 3s
+
+  // Lifecycle
+  onMounted(() => {
+    eventBus.on('alertDanger', (e) => {
+      message.value = e
+    })
+  })
 </script>
 <template>
-  <div class="flex items-center p-3.5 rounded text-danger bg-danger-light absolute right-5 bottom-5 z-[9999]">
+  <div
+    class="items-center p-3.5 rounded text-danger bg-danger-light absolute right-5 bottom-5 z-[9999]"
+    :class="!message.length ? 'hidden' : 'flex'"
+  >
     <span class="pr-2 pl-2">
-      <strong class="mr-1 ml-1">Danger!</strong>Lorem Ipsum is simply dummy text of the printing.
+      {{ message }}
     </span>
     <button type="button" class="ml-auto mr-auto hover:opacity-80">
         <icon-close class="w-5 h-5" />
