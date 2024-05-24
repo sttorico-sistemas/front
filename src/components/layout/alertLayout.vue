6fc873cda16ed2791713b6f28ab2ea549f94cc83
Moises <moises_system@yahoo.com.br>
2024-05-24 03:26:06 +0000

    feat: finalizado componente de alertLayout

--- src/components/layout/alertLayout.vue
+++ src/components/layout/alertLayout.vue
@@ -1,33 +1,66 @@
 <script lang="ts" setup>
-  import { reactive, ref, onMounted, inject } from 'vue'
+  import { reactive, ref, onMounted, computed, inject } from 'vue'
 
   // Icons
   import IconClose from '@icons/iconClose.vue'
+  import IconCheck from '@icons/iconCheck.vue'
+  import IconInfo from '@icons/iconInfo.vue'
 
   // Declarações
   const eventBus = inject('eventBus')
   const message = ref<string>('')
+  const type = ref<string>('info')
 
   // Scripts
-  // remover apos 3s
+  const showAlert = (e) => {
+    message.value = e.message
+    type.value = e.type
+    setTimeout(() => message.value = '', 4000)
+  }
 
-  // Lifecycle
   onMounted(() => {
-    eventBus.on('alertDanger', (e) => {
-      message.value = e
-    })
+    eventBus.on('alert', showAlert)
   })
+
+  const setupAlert = () => {
+    switch (type.value) {
+      case 'success':
+        return {
+          color: 'text-success bg-success-light',
+          icon: IconCheck
+        }
+      case 'danger':
+        return {
+          color: 'text-danger bg-danger-light',
+          icon: IconClose
+        }
+      case 'warning':
+        return {
+          color: 'text-warning bg-warning-light',
+          icon: IconInfo
+        }
+      default:
+        return {
+          color: 'text-info bg-info-light',
+          icon: IconInfo
+        }
+    }
+  }
 </script>
 <template>
   <div
-    class="items-center p-3.5 rounded text-danger bg-danger-light absolute right-5 bottom-5 z-[9999]"
-    :class="!message.length ? 'hidden' : 'flex'"
+    class="flex items-center p-3.5 transition ease-linear duration-1000 rounded absolute right-5 bottom-5 z-[9999]"
+    :class="[!message.length ? 'translate-x-[1000px]' : 'translate-x-0', setupAlert().color]"
   >
     <span class="pr-2 pl-2">
       {{ message }}
     </span>
-    <button type="button" class="ml-auto mr-auto hover:opacity-80">
-        <icon-close class="w-5 h-5" />
+    <button
+      type="button"
+      class="ml-auto mr-auto hover:opacity-80"
+      @click="message = ''"
+    >
+        <component :is="setupAlert().icon" />
     </button>
   </div>
 </template>
