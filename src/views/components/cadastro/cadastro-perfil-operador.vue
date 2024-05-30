e161337df9053b496a2288addbd8fd329a32c89d
Moises <moises_system@yahoo.com.br>
2024-05-30 18:10:37 +0000

    ref: melhoria no componente para adicionar açoes

---
+++ src/views/components/cadastro/cadastro-perfil-operador.vue
@@ -0,0 +1,36 @@
+<script lang="ts" setup>
+	import { reactive, ref } from 'vue'
+
+	// Componentes
+  import VueCollapsible from 'vue-height-collapsible/vue3'
+	import breadcrumbs from '@components/layout/breadcrumbsLayout.vue'
+	import titulo from '@components/layout/tituloLayout.vue'
+  import modalLayout from '@components/layout/modalLayout.vue'
+  import LabelInput from '@components/layout/forms/inputs/inputLabel.vue'
+  import LabelSelect from '@components/layout/forms/inputs/selectLabel.vue'
+
+  import ConsultasCadastroPessoa from '../consultas/consultas-cadastro-pessoa/consultas-cadastro-pessoa.vue'
+  import CadastroPerfilOperador from './cadastro-perfil-operador/cadastro-perfil-operador.vue'
+
+	// Icons
+  import IconAdd from '@icons/iconAdd.vue'
+  import IconCaretDown from '@icons/iconCaretDown.vue'
+
+	// Declarações
+  const isDisabled = ref(true)
+  const isOpenDialog = ref(false)
+  const accordians = reactive({
+    perfilOperador: false,
+  })
+
+</script>
+<template>
+	<main>
+		<breadcrumbs :paginas="['Consultas', 'Perfil Operador']" />
+
+    <cadastro-perfil-operador
+      add-button
+      class="mt-6"
+    />
+	</main>
+</template>
