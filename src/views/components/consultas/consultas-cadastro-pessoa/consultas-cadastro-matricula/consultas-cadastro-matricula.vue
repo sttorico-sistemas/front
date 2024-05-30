e7743d22e54c693424350f22025d2c2bc5c6931e
Moises <moises_system@yahoo.com.br>
2024-05-30 21:54:39 +0000

    feat: criado componente para cadastrar matricula

---
+++ src/views/components/consultas/consultas-cadastro-pessoa/consultas-cadastro-matricula/consultas-cadastro-matricula.vue
@@ -0,0 +1,127 @@
+<script lang="ts" setup>
+  // Core
+  import { reactive, ref, onMounted, inject } from 'vue'
+
+  // Props
+  const props = defineProps({
+    disabled: {
+      type: Boolean,
+      default: true,
+    }
+  })
+
+	// Componentes
+  import titulo from '@components/layout/tituloLayout.vue'
+  import LabelInput from '@components/layout/forms/inputs/inputLabel.vue'
+  import LabelSelect from '@components/layout/forms/inputs/selectLabel.vue'
+
+  import ConsultasCadastroPessoaContato from './consultas-cadastro-pessoa-contato/consultas-cadastro-pessoa-contato.vue'
+  import ConsultasCadastroPessoaEndereco from './consultas-cadastro-pessoa-endereco/consultas-cadastro-pessoa-endereco.vue'
+
+  // Icons
+  import IconEdit from '@icons/iconEdit.vue'
+
+	// Declarações
+  const isDisabled = ref<boolean>(true)
+
+	// Script
+
+  const emits = defineEmits(['btnSave', 'btnCancelar'])
+
+  onMounted(() => {
+    isDisabled.value = props.disabled
+  })
+</script>
+<template>
+	<main>
+    <div class="panel">
+      <titulo title="Matrículas" class="mb-6" />
+      <div class="flex flex-col md:flex-row flex-wrap gap-2.5">
+        <label-input
+          id="matricula"
+          label="Matrícula"
+          class-label="text-sm"
+          class-input="md:max-w-[200px]"
+          layout="row"
+        />
+        <label-input
+          id="consignante"
+          label="Consignante"
+          class-label="text-sm"
+          class-input="md:min-w-[200px]"
+          layout="row"
+        />
+        <label-input
+          id="averbador"
+          label="Averbador"
+          class-label="text-sm"
+          class-input="md:max-w-[200px]"
+          layout="row"
+        />
+        <label-input
+          id="categoria"
+          label="Categoria"
+          class-label="text-sm"
+          class-input="md:w-[200px]"
+          layout="row"
+        />
+        <label-input
+          id="regime_contratacao"
+          label="Regime de Contratação"
+          class-label="text-sm"
+          class-input="md:w-[200px]"
+          layout="row"
+        />
+        <label-input
+          id="status_funcional"
+          label="Status Funcional"
+          class-label="text-sm"
+          class-input="md:w-[120px]"
+          layout="row"
+        />
+        <label-input
+          id="cargo"
+          label="Cargo"
+          class-label="text-sm"
+          class-input="md:w-[200px]"
+          layout="row"
+        />
+        <label-input
+          id="unidade"
+          label="Unidade"
+          class-label="text-sm"
+          class-input="md:w-[200px]"
+          layout="row"
+        />
+        <label-input
+          id="orgao"
+          label="Órgão"
+          class-label="text-sm"
+          class-input="md:w-[200px]"
+          layout="row"
+        />
+        <label-input
+          id="lotacao"
+          label="Lotação"
+          class-label="text-sm"
+          class-input="md:w-[200px]"
+          layout="row"
+        />
+        <label-input
+          id="admissao"
+          label="Data Admissão"
+          class-label="text-sm"
+          class-input="md:w-[150px]"
+          layout="row"
+        />
+        <label-input
+          id="desligamento"
+          label="Data Desligamento"
+          class-label="text-sm"
+          class-input="md:w-[150px]"
+          layout="row"
+        />
+      </div>
+    </div>
+	</main>
+</template>
