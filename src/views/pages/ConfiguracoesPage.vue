775231f5ce08a026495d5d4b21443bad7f406c45
Moises <moises_system@yahoo.com.br>
2024-08-26 23:22:53 -0300

    feat: criado page para configuraçoes

---
+++ src/views/pages/ConfiguracoesPage.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup>
+	import { useRoute } from 'vue-router'
+	import { onMounted, shallowRef, watch } from 'vue'
+
+	import TabelasAuxiliares from '../components/configuracoes/tabelas-auxiliares.vue'
+
+	const route = useRoute()
+
+	const currentComponent = shallowRef<any>(null)
+
+	watch([route, currentComponent], () => {
+		switch (route.name) {
+			case 'configuracoes-tabelas-auxiliares':
+				currentComponent.value = TabelasAuxiliares
+				break
+		}
+	})
+
+	onMounted(() => {
+		currentComponent.value = route.name
+	})
+</script>
+
+<template>
+	<component :is="currentComponent" />
+</template>
