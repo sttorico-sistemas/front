669e31a226daeb4ff67f09d3bdebb669c3acaba9
Moises <moises_system@yahoo.com.br>
2024-09-01 19:40:42 -0300

    criação de componente de filtros para a tela

---
+++ src/views/components/consultas/consultas-cadastro-pessoa/consultas-filtros-consignador/consultas-filtros-simples.vue
@@ -0,0 +1,66 @@
+<script lang="ts" setup>
+  // Core
+  import { reactive, ref } from 'vue'
+
+	// Componentes
+  import LabelInput from '@components/layout/forms/inputs/inputLabel.vue'
+  import LabelSelect from '@components/layout/forms/inputs/selectLabel.vue'
+
+	// Icons
+  import IconClear from '@icons/iconClear.vue'
+  import IconFilter from '@icons/iconFilter.vue'
+
+	// Declarações
+
+	// Script
+
+</script>
+<template>
+	<main>
+    <div class="panel py-4 mt-6 bg-[#EAEAEC]">
+      <form class="flex justify-between items-center">
+        <div class="flex flex-col md:flex-row gap-4 items-center">
+          <label-select
+            id="tipo_instituicao"
+            label="Tipo de Instituição"
+            class-label="text-[10px]"
+            class-select="w-full md:w-[255px]"
+            layout="row"
+            :options="['consignante 1', 'consignante 2']"
+          />
+
+          <label-select
+            id="consignante_master"
+            label="Consignante Master"
+            class-label="text-[10px]"
+            class-wrapper="w-full md:w-[255px]"
+            layout="row"
+            :options="['consignante 1', 'consignante 2']"
+          />
+
+          <label-select
+            id="consignante"
+            label="Consignante"
+            class-label="text-[10px]"
+            class-wrapper="w-full md:w-[255px]"
+            layout="row"
+            :options="['consignante 1', 'consignante 2']"
+          />
+
+          <label-select
+            id="averbador"
+            label="Averbador"
+            class-label="text-[10px]"
+            class-wrapper="w-full md:w-[255px]"
+            layout="row"
+            :options="['consignante 1', 'consignante 2']"
+          />
+        </div>
+        <div class="flex items-center gap-2.5 mt-4">
+          <icon-clear class="w-6 h-6 text-primary_3-table" />
+          <icon-filter class="w-6 h-6 text-primary_3-table" />
+        </div>
+      </form>
+    </div>
+	</main>
+</template>
