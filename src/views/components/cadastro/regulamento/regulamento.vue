201926c2bc0b139a66d92f07a924cc0801d93449
Moises <moises_system@yahoo.com.br>
2024-09-01 17:48:33 -0300

    feat: criar listagem para regulamento

---
+++ src/views/components/cadastro/regulamento/regulamento.vue
@@ -0,0 +1,227 @@
+<script lang="ts" setup>
+	import Vue3Datatable from '@bhplugin/vue3-datatable'
+	import { reactive, ref } from 'vue'
+
+	// Componentes
+	import titulo from '@components/layout/tituloLayout.vue'
+	import modalLayout from '@components/layout/modalLayout.vue'
+
+  import ConsultasExport from '../../consultas/consultas-export/consultas-export.vue'
+  import CadastrarRegulamento from '../modal-cadastrar-regulamento/modal-cadastrar-regulamento.vue'
+
+	// Icons
+  import IconDoc from '@icons/iconFile.vue'
+	import IconAdd from '@icons/iconAdd.vue'
+	import IconEdit from '@icons/iconEdit.vue'
+	import IconDelete from '@icons/iconDelete.vue'
+  import IconBlock from '@icons/iconBlock.vue'
+	import IconCheck from '@icons/iconCheck.vue'
+  import IconEye from '@icons/iconEye.vue'
+  import IconPrinter from '@icons/iconPrinter.vue'
+  import IconClear from '@icons/iconClear.vue'
+
+	// Declarações
+  const isOpenDialog = ref<boolean>(false);
+	const selected = reactive<{ type: string, label: string }>({
+		type: '',
+		label: '',
+	})
+	const servico = ref<string>('')
+	const cols = reactive([
+		{ field: 'id', title: '#', hide: true, sort: false, },
+		{ field: 'servicos', title: 'Serviços', hide: false, sort: false, },
+		{ field: 'regra', title: 'Regra', hide: false, sort: false, },
+		{ field: 'delimitador', title: 'Delimitador', hide: false, sort: false, },
+		{ field: 'vlr_delimitado', title: 'Vlr. Delimitado', hide: false, sort: false, },
+		{ field: 'unid_delimitada', title: 'Unid. Delimitada', hide: false, sort: false, },
+		{ field: 'dt_inicio', title: 'Dt Início', hide: false, sort: false, },
+		{ field: 'dt_final', title: 'Dt Final', hide: false, sort: false, },
+		{ field: 'status', title: 'Status', hide: false, sort: false, },
+		{ field: 'actions', title: 'Ações', hide: false, sort: false, },
+	])
+	const rows = reactive([
+		{
+			id: 1,
+      servicos: 'Empréstimos',
+      regra: 'Prazo Máximo Descto',
+      delimitador: 'Meses',
+      vlr_delimitado: 'Menor que, incluso',
+      unid_delimitada: '120',
+      dt_inicio: '20/10/2015',
+      dt_final: '20/12/2020',
+      status: 'Ativo',
+    },
+	])
+
+  // Scripts
+  const clearFilter = () => {
+		servico.value = ''
+
+		selected.label = ''
+		selected.type = ''
+	}
+
+  const color = (value: string) => {
+		switch (value) {
+			case 'Ativo':
+				return 'bg-success'
+			case 'Inativo':
+				return 'bg-warning'
+		}
+	}
+
+  const filtered = (value: string = '') => {
+		if (value === '') return rows
+
+		if (selected.type === 'servico')
+			return rows.filter((item: any) => item.grupo_margem === value)
+	}
+
+	const parseCols = (): Array<object> => {
+		return [
+			{ field: 'id', title: '#', hide: true, sort: false, },
+			{ field: 'servicos', title: 'Serviços', hide: false, sort: false, },
+			{ field: 'regra', title: 'Regra', hide: false, sort: false, },
+			{ field: 'delimitador', title: 'Delimitador', hide: false, sort: false, },
+			{ field: 'vlr_delimitado', title: 'Vlr. Delimitado', hide: false, sort: false, },
+			{ field: 'unid_delimitada', title: 'Unid. Delimitada', hide: false, sort: false, },
+			{ field: 'dt_inicio', title: 'Dt Início', hide: false, sort: false, },
+			{ field: 'dt_final', title: 'Dt Final', hide: false, sort: false, },
+			{ field: 'status', title: 'Status', hide: false, sort: false, },
+		]
+	}
+</script>
+
+<template>
+	<main>
+		<div class="panel">
+			<div
+				class="flex flex-wrap justify-between md:items-center md:flex-row flex-col mb-5 gap-5"
+			>
+        <div class="flex items-center gap-14">
+					<titulo title="Regras para Consignações" />
+					<button @click="isOpenDialog = true" v-tippy:right>
+						<icon-add />
+					</button>
+					<tippy target="right" placement="right"
+						>Cadastre uma nova regra para consignações</tippy
+					>
+				</div>
+
+        <div
+					class="header_actions flex items-center gap-5"
+				>
+					<multiselect
+						v-model="servico"
+						:options="['Empréstimos']"
+						class="custom-multiselect min-w-[200px]"
+						placeholder="Tipo Serviço"
+						:searchable="false"
+						:preselect-first="false"
+						:allow-empty="false"
+						selected-label=""
+						select-label=""
+						deselect-label=""
+						@select="(selected.label = $event), (selected.type = 'servico')"
+					/>
+
+					<div>
+						<button
+							v-tippy:top
+							type="button"
+							class="text-xs m-1"
+							@click="clearFilter()"
+						>
+							<icon-clear class="w-5 h-5 text-primary_3-table" />
+						</button>
+						<tippy target="top" placement="top">Limpar pesquisa</tippy>
+					</div>
+
+					<div class="w-5 h-5">
+						<consultas-export
+							v-tippy:top
+							:cols="parseCols()"
+							:rows="rows"
+							filename="Normativos Cadastrados"
+							export-type="print"
+						>
+							<template #icon>
+								<icon-printer class="w-5 h-5" />
+							</template>
+						</consultas-export>
+						<tippy target="top" placement="top">Imprimir</tippy>
+					</div>
+				</div>
+			</div>
+
+			<div class="datatable">
+				<vue3-datatable
+					:rows="filtered(selected.label)"
+					:columns="cols"
+					:total-rows="filtered(selected.label).length"
+					:sortable="true"
+					skin="whitespace-nowrap bh-table-striped mb-5"
+					no-data-content="Nenhum dado foi encontrado"
+					pagination-info="Mostrando {0} a {1} de {2} entradas"
+				>
+          <template #status="data">
+            <span
+              class="flex justify-center badge !w-[80px] h-[22px]"
+              :class="color(data.value.status)"
+              >{{ data.value.status }}</span
+            >
+          </template>
+          <template #actions="data">
+						<div class="flex gap-2">
+              <div>
+								<button
+									v-tippy:right
+									type="button"
+									class="text-xs m-1"
+								>
+									<icon-eye class="w-5 h-5 text-primary_3-table" />
+								</button>
+								<tippy target="right" placement="right"
+									>Visualizar</tippy
+								>
+							</div>
+              <div>
+								<button
+									v-tippy:right
+									type="button"
+									class="text-xs m-1"
+								>
+									<icon-edit class="w-5 h-5 text-primary_3-table" />
+								</button>
+								<tippy target="right" placement="right"
+									>Editar</tippy
+								>
+							</div>
+              <div>
+								<button
+									v-tippy:right
+									type="button"
+									class="text-xs m-1"
+								>
+									<icon-block v-if="data.value.status === 'Ativo'" class="w-5 h-5 text-primary_3-table" />
+									<icon-check v-else class="w-5 h-5 text-primary_3-table" />
+								</button>
+								<tippy target="right" placement="right"
+									>{{ data.value.status === 'Ativo' ? 'Inativo' : 'Ativo' }}</tippy
+								>
+							</div>
+            </div>
+					</template>
+        </vue3-datatable>
+			</div>
+		</div>
+    <modal-layout
+      title="Cadastrar Regulamento"
+      :is-open="isOpenDialog"
+      size="max-w-[600px]"
+      @btn-close="isOpenDialog = false"
+    >
+      <cadastrar-regulamento @btn-cancelar="isOpenDialog = false" />
+    </modal-layout>
+	</main>
+</template>
