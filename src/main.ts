eb1b4ebd5eb9a954fb84f92eae60ebaf204a0dcf
Felipe <felipeyslaok@gmail.com>
2024-10-09 13:12:01 -0300

    setup pinia

--- src/main.ts
+++ src/main.ts
@@ -13,8 +13,10 @@ import VueApexCharts from 'vue3-apexcharts'
 import Popper from 'vue3-popper'
 import JsonExcel from 'vue-json-excel3'
 import Multiselect from '@suadelabs/vue3-multiselect'
+import { createPinia } from 'pinia'
 
 const app = createApp(App)
+const pinia = createPinia()
 
 app.component('default-layout', DefaultLayout);
 app.component('app-layout', AppLayout);
@@ -22,6 +24,7 @@ app.component('Popper', Popper)
 app.component('DownloadExcel', JsonExcel)
 app.component('Multiselect', Multiselect)
 
+app.use(pinia)
 app.use(createHead())
 app.use(TippyPlugin)
 app.use(Maska)
