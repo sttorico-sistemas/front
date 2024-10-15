036aa408f1a6ec6dee871fde199244fd098b78c8
Moises <moises_system@yahoo.com.br>
2024-10-15 00:27:58 -0300

    ref: refatoração de telas

--- src/views/components/consultas/consultas-cadastro-pessoa/consultas-cadastro-pessoa-endereco/consultas-cadastro-pessoa-endereco.vue
+++ src/views/components/consultas/consultas-cadastro-pessoa/consultas-cadastro-pessoa-endereco/consultas-cadastro-pessoa-endereco.vue
@@ -1,6 +1,5 @@
 <script lang="ts" setup>
   // Core
-  import { reactive, ref, inject } from 'vue'
 
   // Componentes
   import titulo from '@components/layout/tituloLayout.vue'
@@ -9,115 +8,60 @@
   import LabelSelect from '@components/layout/forms/inputs/selectLabel.vue'
 
   // Icons
-  import IconAdd from '@icons/iconAdd.vue'
-  import IconClose from '@icons/iconClose.vue'
 
   // Declarações
-  const eventBus = inject('eventBus')
-  const isDisabled = ref<boolean>(false)
-  const enderecos = reactive([
-    {
-      tipoEndereco: '',
-      endereco: '',
-      cidade: '',
-      uf: '',
-    }
-  ])
 
   // Script
-  const addEndereco = () => {
-    if (enderecos.length >= 3) {
-      eventBus.emit('alert', {
-        type: 'danger',
-        message: 'Limite máximo de contato é de 3'
-      })
-
-      return false
-    }
-
-    if (!enderecos[0].endereco.length && !enderecos[0].cidade.length) {
-      eventBus.emit('alert', {
-        type: 'danger',
-        message: 'Preencha os campos obrigatórios!'
-      })
-
-      return false
-    }
-
-    enderecos.push({
-      tipoEndereco: '',
-      endereco: '',
-      cidade: '',
-      uf: '',
-    })
-  }
-
-  const removeEndereco = (index: number) => {
-    if (enderecos.length === 1) {
-      eventBus.emit('alert', {
-        type: 'danger',
-        message: 'Não foi possivel remover contato'
-      })
-      return false
-    }
-
-    enderecos.splice(index, 1)
-  }
+  const emits = defineEmits(['btnSave', 'btnCancelar'])
 </script>
 <template>
   <div class="panel my-3">
-    <titulo title="Endereços" class="mb-6" />
-
-    <div
-      v-for="(endereco, id) in enderecos"
-      :key="`endereco-${id}`"
-      class="flex-col md:flex-row flex gap-2.5"
-    >
-      <label-select
-        id="tipo_endereco"
-        label="Tipo Endereço"
-        :disabled="isDisabled"
-        class-label="text-sm"
-        class-select="md:w-[200px]"
-        layout="row"
-        :options="['Rua', 'Comunidade', 'Condominio']"
-      />
-      <label-input
-        v-model="endereco.endereco"
-        id="endereco"
-        label="Endereço"
-        :disabled="isDisabled"
-        class-label="text-sm"
-        class-input="md:min-w-[545px]"
-        layout="row"
-      />
-      <label-input
-        v-model="endereco.cidade"
-        id="cidade"
-        label="Cidade"
-        :disabled="isDisabled"
-        class-label="text-sm"
-        class-input="md:max-w-[300px]"
-        layout="row"
-      />
-      <label-select
-        id="uf"
-        label="UF"
-        :disabled="isDisabled"
-        class-label="text-sm"
-        class-select="md:w-[70px]"
-        layout="row"
-        :options="['SP', 'RJ', 'MG', 'ES']"
-      />
-      <div class="flex items-center gap-1">
-        <button @click="addEndereco(endereco, enderecos)" v-tippy:right class="flex self-end mb-2">
-          <icon-add />
-        </button>
-        <button @click="removeEndereco(index)"  class="flex self-end mb-2">
-          <icon-close />
-        </button>
-        <tippy target="right" placement="right">Cadastre um novo Endereco</tippy>
-      </div>
-    </div>
+    <titulo title="Dados do Endereço" class="mb-6" />
+    <label-select
+      id="tipo_endereco"
+      label="Tipo Endereço"
+      class-label="text-sm"
+      class-select="md:max-w-[200px]"
+      layout="col"
+      :options="['Rua', 'Comunidade', 'Condominio']"
+    />
+    <label-input
+      id="endereco"
+      label="Endereço"
+      class-label="text-sm"
+      class-input="md:max-w-[400px]"
+      layout="col"
+    />
+    <label-input
+      id="bairro"
+      label="Bairro"
+      class-label="text-sm"
+      class-input="md:max-w-[300px]"
+      layout="col"
+    />
+    <label-select
+      id="uf"
+      label="UF"
+      class-label="text-sm"
+      class-select="md:max-w-[70px]"
+      layout="col"
+      :options="['SP', 'RJ', 'MG', 'ES']"
+    />
+    <label-select
+      id="cidade"
+      label="Cidade"
+      class-label="text-sm"
+      class-select="md:max-w-[200px]"
+      layout="col"
+      :options="['Rio de Janeiro']"
+    />
+  </div>
+  <div class="flex justify-center items-center gap-12 mt-8">
+    <button
+      type="button"
+      class="w-[86px] btn border border-primary_3-table shadow-none text-primary_3-table text-xs"
+      @click="emits('btnCancelar', false)"
+    >Cancelar</button>
+    <button type="button" class="w-[86px] btn bg-primary_3-table text-white text-xs">Salvar</button>
   </div>
 </template>
