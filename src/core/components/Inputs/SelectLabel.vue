aaabcf495ec7c08d01a7b6fa0351f910865c060f
Felipe <felipeyslaok@gmail.com>
2024-10-24 00:04:39 -0300

    feature: new dialog component

--- src/core/components/Inputs/SelectLabel.vue
+++ src/core/components/Inputs/SelectLabel.vue
@@ -29,23 +29,23 @@ const classDisabled = computed(() => {
   return props.disabled ? 'disabled:pointer-events-none disabled:bg-[#eee] dark:disabled:bg-[#1b2e4b] cursor-not-allowed' : ''
 })
 </script>
+
 <template>
   <div v-if="layout === 'row'" :class="classWrapper">
     <label for="id" class="mb-0" :class="classLabel">{{ label }}</label>
-    <select :id="id" class="form-select h-[38px] flex-1" :class="[classSelect, classDisabled]" :required="required"
-      :disabled="disabled">
+    <select :id="id" class="form-select h-[38px] flex-1 truncate overflow-hidden text-ellipsis whitespace-nowrap"
+      :class="[classSelect, classDisabled]" :required="required" :disabled="disabled">
       <option value="">{{ placeholder }}</option>
-      <option v-for="option in options" :value="value">{{ option }}</option>
+      <option v-for="option in options" :key="option" :value="value">{{ option }}</option>
     </select>
   </div>
 
   <div v-if="layout === 'col'" class="flex sm:flex-row flex-col items-center mb-3">
-    <label :for="id" class="mb-0 md:w-1/5 mr-2 w-full text-left" :class="classLabel">{{ label
-      }}</label>
-    <select :id="id" class="form-select h-[38px] flex-1" :class="[classSelect, classDisabled]" :required="required"
-      :disabled="disabled">
+    <label :for="id" class="mb-0 md:w-1/5 mr-2 w-full text-left" :class="classLabel">{{ label }}</label>
+    <select :id="id" class="form-select h-[38px] flex-1 truncate overflow-hidden text-ellipsis whitespace-nowrap"
+      :class="[classSelect, classDisabled]" :required="required" :disabled="disabled">
       <option value="">{{ placeholder }}</option>
-      <option v-for="option in options" :value="value">{{ option }}</option>
+      <option v-for="option in options" :key="option" :value="value">{{ option }}</option>
     </select>
   </div>
 </template>
