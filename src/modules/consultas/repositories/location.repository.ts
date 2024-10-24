9eb3f7a1be7e6bbe7fe925b24debb3ebec46d41b
Felipe <felipeyslaok@gmail.com>
2024-10-24 00:10:51 -0300

    feature: load cities

--- src/modules/consultas/repositories/location.repository.ts
+++ src/modules/consultas/repositories/location.repository.ts
@@ -2,6 +2,8 @@ import { useAxios } from "src/core/composables/use_axios";
 import { BaseError } from "src/core/errors/base.error";
 import { LocationState } from "../types/location_state.d";
 import { LocationStateModel } from "../models/location_state.model";
+import { LocationCity } from "../types/location_city";
+import { LocationCityModel } from "../models/location_city.model";
 
 export class LocationRepository {
   private http = useAxios();
@@ -11,6 +13,17 @@ export class LocationRepository {
       const response = await this.http.get('/estados');
       return (response.data.data as []).map(
         (e) => LocationStateModel.fromRecord(e),
+      );
+    } catch (error) {
+      throw BaseError.fromHttpError(error);
+    }
+  }
+
+  async getStateCities(stateId: number): Promise<LocationCity[]> {
+    try {
+      const response = await this.http.get(`/estados/${stateId}/cidades`)
+      return (response.data.data as []).map(
+        (e) => LocationCityModel.fromRecord(e),
       );
     } catch (error) {
       throw BaseError.fromHttpError(error);
