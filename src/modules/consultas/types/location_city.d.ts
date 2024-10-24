91884a1f5a61085636372c5f5928b16cd76df210
Felipe <felipeyslaok@gmail.com>
2024-10-24 00:10:06 -0300

    updated: types

---
+++ src/modules/consultas/types/location_city.d.ts
@@ -0,0 +1,9 @@
+export interface LocationCity {
+  id: number;
+  name: string;
+  stateId: number;
+  ibgeId: number;
+  createdAt: Date;
+  updatedAt: Date;
+  deletedAt?: Date;
+}
--- src/modules/consultas/types/location_state.d.ts
+++ src/modules/consultas/types/location_state.d.ts
@@ -1,7 +1,42 @@
+import { LocationCity } from "./location_city";
+
+export type StateUf =
+  | 'RO'
+  | 'AC'
+  | 'AM'
+  | 'RR'
+  | 'PA'
+  | 'AP'
+  | 'TO'
+  | 'MA'
+  | 'PI'
+  | 'CE'
+  | 'RN'
+  | 'PB'
+  | 'PE'
+  | 'AL'
+  | 'SE'
+  | 'BA'
+  | 'MG'
+  | 'ES'
+  | 'RJ'
+  | 'SP'
+  | 'PR'
+  | 'SC'
+  | 'RS'
+  | 'MS'
+  | 'MT'
+  | 'GO'
+  | 'DF';
+
+export type StateCities = {
+  [key in StateUf]?: LocationCity[];
+};
+
 export interface LocationState {
   id: number;
   name: string;
-  uf: string;
+  uf: StateUf;
   createdAt: string;
   updatedAt: string;
   deletedAt: string;
