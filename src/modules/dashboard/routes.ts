6e0c3cacbb1325806d94e0872fe5192ff5ef0780
Felipe <felipeyslaok@gmail.com>
2024-11-05 21:12:26 -0300

    Hiding dashboard

--- src/modules/dashboard/routes.ts
+++ src/modules/dashboard/routes.ts
@@ -1,10 +1,12 @@
 import { RouteRecordRaw } from "vue-router";
-import DashboardPage from "./pages/DashboardPage.vue";
+// import DashboardPage from "./pages/DashboardPage.vue";
+import Empty from "src/core/components/Empty.vue";
 
 export const dashboradRoutes: RouteRecordRaw[] = [
-  {
-    path: '/',
-    name: 'dashboard',
-    component: DashboardPage,
-  },
+	{
+		path: '/',
+		name: 'dashboard',
+		// component: DashboardPage,
+		component: Empty,
+	},
 ]
